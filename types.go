a86f39d0b83680aa865101f9e227091a5d5198d5
Alex <kudrushow@gmail.com>
2021-12-02 12:11:52 +0200

    feat: log type

--- types.go
+++ types.go
@@ -6,12 +6,22 @@ type Client interface {
 	LogError(context.Context, Message) error
 }
 
+type LogType string
+
+const (
+	Error   LogType = "Error"
+	Info    LogType = "Info"
+	Debug   LogType = "Debug"
+	Warning LogType = "Warning"
+)
+
 type Message struct {
-	XReqID string `json:"x_req_id"`
-	Data   string `json:"data"`
-	File   string `json:"file"`
-	Action string `json:"action"`
-	Error  error  `json:"error"`
+	Type   LogType `json:"type"`
+	XReqID string  `json:"x_req_id"`
+	Data   string  `json:"data"`
+	File   string  `json:"file"`
+	Action string  `json:"action"`
+	Error  error   `json:"error"`
 }
 
 type message struct {
